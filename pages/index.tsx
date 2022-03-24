b3e43ceeb38b42d396e4e3d7db7196c19930b09b
Johannes Krabbe <Johannes.Krabbe@GoKrabbe.de>
2022-03-24 16:51:26 +0100

    fix tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div>
-      <p className='text-3xl underline'> Hallo Niklas </p>
+      <p className='text-3xl font-bold underline'> Hallo Niklas!</p>
     </div>
   )
 }
