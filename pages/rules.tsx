a037e9343bf32173f04d48e54ae084b9a6d4d979
Johannes Krabbe <Johannes.Krabbe@GoKrabbe.de>
2022-03-24 16:53:04 +0100

    added rules page

---
+++ pages/rules.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Rules: NextPage = () => {
+  return (
+    <div>
+      <p className='text-3xl font-bold underline'> This are the rules</p>
+    </div>
+  )
+}
+
+export default Rules
