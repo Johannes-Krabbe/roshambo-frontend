779423f3c58e2bfbab0b4de1c4992471d75d1f31
Niklas Jacobsen <niklasjacobsen4201@gmail.com>
2022-03-24 16:39:10 +0100

    added tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div>
-      <h1> Hallo Niklas </h1>
+      <p className='text-3xl underline'> Hallo Niklas </p>
     </div>
   )
 }
